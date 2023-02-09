b88980104134384c1bbf259a9e9c18d4e39d76f3
Пейко Олег Петрович <pop457045@hotmail.com>
2023-02-09 21:06:42 +0300

    Добавьте файлы проекта.

---
+++ StringCalculator/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace StringCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(13, 28);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(296, 26);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выражение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ответ";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextBox.Location = new System.Drawing.Point(13, 84);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(296, 26);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.calcButton.Location = new System.Drawing.Point(203, 118);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(106, 37);
+            this.calcButton.TabIndex = 2;
+            this.calcButton.Text = "Посчитать";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 163);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button calcButton;
+    }
+}
+
---
+++ StringCalculator/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringCalculator
+{
+    public partial class Form1 : Form
+    {
+
+        char[] opers = new char[] { '+', '-', '*', '/' };
+        char[] prior = new char[] { '(', ')' };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            string inp = inputTextBox.Text;
+            if (corrected(inp))
+            {
+                outputTextBox.Text = StartCalulating(inp).ToString();
+            }
+        }
+
+
+        private bool corrected(string txt)
+        {
+            int skobscount = 0;
+            int textlenght = txt.Length;
+            if (textlenght > 0)
+            {
+                for (int i = 0; i < textlenght; i++)
+                {
+                    if ((i + 1) < textlenght && (opers.Any(s => s == txt[i + 1]) && opers.Any(s => s == txt[i])))
+                    {
+                        MessageBox.Show("Перепроверь операторы");
+                        return false;
+                    }
+                    if (txt[i] == '(') skobscount++;
+                    if (txt[i] == ')') skobscount--;
+                    if (skobscount < 0)
+                    {
+                        MessageBox.Show("Проверьте закрывающие скобки");
+                        return false;
+                    }
+                }
+                if (skobscount > 0)
+                {
+                    MessageBox.Show("Проверьте открывающие скобки");
+                    return false;
+                }
+            }
+            if (opers.Any(s => s == txt[textlenght - 1]))
+            {
+                MessageBox.Show("Перепроверь операторы");
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Начать считать строку
+        /// </summary>
+        /// <param name="inpText">входной текст</param>
+        /// <returns></returns>
+        private double StartCalulating(string inpText)
+        {
+            string workingString = inpText;
+            bool isSplitted = false;
+            double result = double.NaN;
+            if (double.TryParse(inpText, out result))
+                return result;
+            else
+            {
+                result = double.NaN;
+            }
+            //работа со скобками
+            if (inpText.Intersect(prior).Count() > 0)
+            {
+                workingString = PriorCalc(workingString);
+            }
+            // работа с операторами
+            foreach (char oper in opers)
+            {
+                if (!isSplitted)
+                {
+
+                    string[] substrings = workingString.Split(oper);
+                    int sslenght = substrings.Count();
+                    if (sslenght > 1) isSplitted = true;
+                    if ((sslenght > 1) && (result.Equals(double.NaN)))
+                    {
+                        result = StartCalulating(substrings[0]);
+                    }
+                    if (!result.Equals(double.NaN))
+                    {
+                        for (int sstr = 1; sstr < sslenght; sstr++)
+                        {
+                            string str = substrings[sstr];
+                            if (oper == '+')
+                            {
+                                result += StartCalulating(str);
+                            }
+                            if (oper == '-')
+                            {
+                                result -= StartCalulating(str);
+                            }
+                            if (oper == '*')
+                            {
+                                result *= StartCalulating(str);
+                            }
+                            if (oper == '/')
+                            {
+                                result /= StartCalulating(str);
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Подсчёт содержимого в скобках и корректировка выходной строки
+        /// </summary>
+        /// <param name="inpText">входная строка</param>
+        /// <returns></returns>
+        private string PriorCalc (string inpText)
+        {
+            string retval = "";
+            bool priorFinded = false;
+            int textlenght = inpText.Length;
+            int skobscount = 0;
+            string priortext = "";
+            if (textlenght > 0)
+            {
+                for (int i = 0; i < textlenght; i++)
+                {
+                    if (inpText[i] == '(')
+                    {
+                        if (!priorFinded) i++;
+                        priorFinded = true;
+                        skobscount++;
+                    }
+                    if (inpText[i] == ')') skobscount--;
+
+                    if (!priorFinded) retval += inpText[i];
+                    if (priorFinded)
+                    {
+                        if (skobscount > 0)
+                        {
+                            priortext += inpText[i];
+                        }
+                        else
+                        {
+                            retval += (StartCalulating(priortext).ToString());
+                            priortext = "";
+                            priorFinded = false;
+                        }
+                    }
+                }
+            }
+            return retval;
+        }
+    }
+}
